6db30a0293c4ac552920326fef841559ad180844
avishka964 <avishkakapuruge@gmail.com>
2022-03-17 13:32:42 +0530

    update location

--- frontend/src/Screens/LoginScreen.js
+++ frontend/src/Screens/LoginScreen.js
@@ -13,7 +13,7 @@ const LoginScreen = ({ location, history }) => {
   const dispatch = useDispatch()
   const userLogin = useSelector((state) => state.userLogin)
   const { loading, error, userInfo } = userLogin
-  const redirect = location.search ? location.search.split("=")[1] : "/"
+  const redirect = location !== undefined && location.search ? location.search.split("=")[1] : "/"
 
   useEffect(() => {
     if (userInfo) {
